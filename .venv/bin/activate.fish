eca0c8bbe7fb6a775cbe844e65092f106484a963
gui.josepette <gui.josepette@gmail.com>
2024-04-12 17:01:16 +0000

    venv arrumado e flask funcional na idx

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/workspaces/CatalogoFilme/.venv"
+set -gx VIRTUAL_ENV "/home/user/CatalogoFilme/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
