867015796a85be92a273bf3fd3f2f2b716e10c73
gui.josepette <gui.josepette@gmail.com>
2024-04-16 19:44:32 +0000

    config

--- src/config.py
+++ src/config.py
@@ -4,9 +4,13 @@ from database.database import db
 from database.Models import Filme 
 
 
-def configure_all():
-    pass
+def configure_all(app):
+    configure_db()
+    configure_routes(app)
 
 def configure_db():
     db.connect()
     db.create_tables([Filme])
+
+def configure_routes(app):
+    app.register_blueprint(filme_route)
